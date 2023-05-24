a5a4db2341a19fc7f31713f3fc9a7a6db97bc0e2
Fil <concatofilippo94@gmail.com>
2023-05-24 09:21:26 +0200

    works properly

--- js/ajaxRequest.js
+++ js/ajaxRequest.js
@@ -2,13 +2,13 @@ function calculate() {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementsByName("result").innerHTML = this.responseText;
-            // document.getElementsById('result').value = this.responseText;
+            document.getElementById('result').value = this.responseText;
+            console.log(this.responseText);
         }
     };
     const param = `val1=${document.getElementById("firstValue").value}&val2=${document.getElementById("secondValue").value}&op=${document.getElementById("ops").value}`;
-
     xhttp.open("POST", "ajax.php");
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send(param);
+
 }
